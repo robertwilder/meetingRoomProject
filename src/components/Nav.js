b390f9a6def8fb15a1e9c4289a8a0d80ad8953cb
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-12-18 14:05:11 +0000

    i dont know anymore

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+export default function Nav() {
+    return(
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/calendar">Calendar</Link>
+            <Link to="/bookingSystem">BookingSystem</Link>
+            <Link to="/facilities">Facilities</Link>
+        </div>
+    )
+}
